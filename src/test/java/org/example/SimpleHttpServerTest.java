2cd6612457cbf4dd1d95cd06aa8a8d33f968a0b5
Marcel Luethi <marcel.luethi@unibas.ch>
2024-09-24 10:10:52 +0200

    debuggin

--- src/test/java/org/example/SimpleHttpServerTest.java
+++ src/test/java/org/example/SimpleHttpServerTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
